43150f6938cfa6f7396b333bf0e92260d394f33f
holocaster <paulorobertosp@gmail.com>
2021-06-28 13:13:08 -0300

    Colocando valores default de paginacao

--- src/main/java/br/gov/inst/atlan/userapi/rest/v1/UserRest.java
+++ src/main/java/br/gov/inst/atlan/userapi/rest/v1/UserRest.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
-import javax.ws.rs.core.Response;
 import java.net.URI;
 import java.util.UUID;
 
@@ -22,11 +21,22 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class UserRest {
 
+    private static final Integer DEFAULT_PAGE_NUMBER = 0;
+    private static final Integer DEFAULT_PAGE_SIZE = 10;
+
     private final UserService userService;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UserDTOPagedList> listUsers(@RequestParam(value = "pageNumber", required = false) Integer pageNumber,
                                                       @RequestParam(value = "pageSize", required = false) Integer pageSize) {
+
+        if (pageNumber == null || pageNumber < 0) {
+            pageNumber = DEFAULT_PAGE_NUMBER;
+        }
+
+        if (pageSize == null || pageSize < 1) {
+            pageSize = DEFAULT_PAGE_SIZE;
+        }
 
         UserDTOPagedList userDTOs = this.userService.listUsers(PageRequest.of(pageNumber, pageSize));
 
